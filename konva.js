ad265f6a434eb0981c64d8addd76d3339c2e9a14
Lloyd Escoto <42109272+lloydescoto22@users.noreply.github.com>
2018-09-15 21:31:08 +0800

    Added Text Name for Elements

--- konva.js
+++ konva.js
@@ -13,6 +13,14 @@ stage.add(layer);
 
 stage.on('click', function (e) {
   var position = JSON.parse(JSON.stringify(stage.getPointerPosition()));
+  var simpleText = new Konva.Text({
+      x: position['x'] - 11,
+      y: position['y'] - 15,
+      text: 'H',
+      fontSize: 30,
+      fontFamily: 'Arial',
+      fill: 'white'
+  });
   var molecule = new Konva.Circle({
 	x: position['x'],
 	y: position['y'],
@@ -25,6 +33,15 @@ stage.on('click', function (e) {
 	y: position['y'],
 	innerRadius: 40,
 	outerRadius: 41,
+	fill: 'yellow',
+	stroke: 'black',
+	strokeWidth: 1
+  });
+  var ringMolecule2 = new Konva.Ring({
+	x: position['x'],
+	y: position['y'],
+	innerRadius: 50,
+	outerRadius: 51,
 	fill: 'yellow',
 	stroke: 'black',
 	strokeWidth: 1
@@ -46,7 +63,9 @@ stage.on('click', function (e) {
 	  atom.rotate(angleDiff);
   }, layer);
   layer.add(molecule).draw();
+  layer.add(simpleText).draw();
   layer.add(ringMolecule).draw();
+  layer.add(ringMolecule2).draw();
   layer.add(atom).draw();
   anim.start();
 });
