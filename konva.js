2b50442a7befea7989dcf00fc6c9b2295c2f78a8
Lloyd Escoto <42109272+lloydescoto22@users.noreply.github.com>
2018-09-29 23:13:24 +0800

    Added Elements Shell Electrons List

--- konva.js
+++ konva.js
@@ -185,6 +185,127 @@ function covalentBonding(elements)
 	}
 }
 
+var elementShell = {
+	1: [1],
+	2: [2],
+	3: [2, 1],
+	4: [2, 2],
+	5: [2, 3],
+	6: [2, 4],
+	7: [2, 5],
+	8: [2, 6],
+	9: [2, 7],
+	10: [2, 8],
+	11: [2, 8, 1],
+	12: [2, 8, 2],
+	13: [2, 8, 3],
+	14: [2, 8, 4],
+	15: [2, 8, 5],
+	16: [2, 8, 6],
+	17: [2, 8, 7],
+	18: [2, 8, 8],
+	19: [2, 8, 8, 1],
+	20: [2, 8, 8, 2],
+	21: [2, 8, 9, 2],
+	22: [2, 8, 10,2],
+	23: [2, 8, 11, 2],
+	24: [2, 8, 13, 1],
+	25: [2, 8, 13, 2],
+	26: [2, 8, 14, 2],
+	27: [2, 8, 15, 2],
+	28: [2, 8, 16, 2],
+	29: [2, 8, 18, 1],
+	30: [2, 8, 18, 2],
+	31: [2, 8, 18, 3],
+	32: [2, 8, 18, 4],
+	33: [2, 8, 18, 5],
+	34: [2, 8, 18, 6],
+	35: [2, 8, 18, 7],
+	36: [2, 8, 18, 8],
+	37: [2, 8, 18, 8, 1],
+	38: [2, 8, 18, 8, 2],
+	39: [2, 8, 18, 9, 2],
+	40: [2, 8, 18, 10, 2],
+	41: [2, 8, 18, 12, 1],
+	42: [2, 8, 18, 13, 1],
+	43: [2, 8, 18, 13, 2],
+	44: [2, 8, 18, 15, 1],
+	45: [2, 8, 18, 16, 1],
+	46: [2, 8, 18, 18, 0],
+	47: [2, 8, 18, 18, 1],
+	48: [2, 8, 18, 18, 2],
+	49: [2, 8, 18, 18, 3],
+	50: [2, 8, 18, 18, 4],
+	51: [2, 8, 18, 18, 5],
+	52: [2, 8, 18, 18, 6],
+	53: [2, 8, 18, 18, 7],
+	54: [2, 8, 18, 18, 8],
+	55: [2, 8, 18, 18, 8, 1],
+	56: [2, 8, 18, 18, 8, 2],
+	57: [2, 8, 18, 18, 9, 2],
+	58: [2, 8, 18, 19, 9, 2],
+	59: [2, 8, 18, 21, 8, 2],
+	60: [2, 8, 18, 22, 8, 2],
+	61: [2, 8, 18, 23, 8, 2],
+	62: [2, 8, 18, 24, 8, 2],
+	63: [2, 8, 18, 25, 8, 2],
+	64: [2, 8, 18, 25, 9, 2],
+	65: [2, 8, 18, 27, 8, 2],
+	66: [2, 8, 18, 28, 8, 2],
+	67: [2, 8, 18, 29, 8, 2],
+	68: [2, 8, 18, 30, 8, 2],
+	69: [2, 8, 18, 31, 8, 2],
+	70: [2, 8, 18, 32, 8, 2],
+	71: [2, 8, 18, 32, 9, 2],
+	72: [2, 8, 18, 32, 10, 2],
+	73: [2, 8, 18, 32, 11, 2],
+	74: [2, 8, 18, 32, 12, 2],
+	75: [2, 8, 18, 32, 13, 2],
+	76: [2, 8, 18, 32, 14, 2],
+	77: [2, 8, 18, 32, 15, 2],
+	78: [2, 8, 18, 32, 17, 1],
+	79: [2, 8, 18, 32, 18, 1],
+	80: [2, 8, 18, 32, 18, 2],
+	81: [2, 8, 18, 32, 18, 3],
+	82: [2, 8, 18, 32, 18, 4],
+	83: [2, 8, 18, 32, 18, 5],
+	84: [2, 8, 18, 32, 18, 6],
+	85: [2, 8, 18, 32, 18, 7],
+	86: [2, 8, 18, 32, 18, 8],
+	87: [2, 8, 18, 32, 18, 8, 1],
+	88: [2, 8, 18, 32, 18, 8, 2],
+	89: [2, 8, 18, 32, 18, 9, 2],
+	90: [2, 8, 18, 32, 18, 10, 2],
+	91: [2, 8, 18, 32, 20, 9, 2],
+	92: [2, 8, 18, 32, 21, 9, 2],
+	93: [2, 8, 18, 32, 22, 9, 2],
+	94: [2, 8, 18, 32, 24, 8, 2],
+	95: [2, 8, 18, 32, 25, 8, 2],
+	96: [2, 8, 18, 32, 25, 9, 2],
+	97: [2, 8, 18, 32, 27, 8, 2],
+	98: [2, 8, 18, 32, 28, 8, 2],
+	99: [2, 8, 18, 32, 29, 8, 2],
+	100: [2, 8, 18, 32, 30, 8, 2],
+	101: [2, 8, 18, 32, 31, 8, 2],
+	102: [2, 8, 18, 32, 32, 8, 2],
+	103: [2, 8, 18, 32, 32, 8, 3],
+	104: [2, 8, 18, 32, 32, 10, 2],
+	105: [2, 8, 18, 32, 32, 11, 2],
+	106: [2, 8, 18, 32, 32, 12, 2],
+	107: [2, 8, 18, 32, 32, 13, 2],
+	108: [2, 8, 18, 32, 32, 14, 2],
+	109: [2, 8, 18, 32, 32, 15, 2],
+	110: [2, 8, 18, 32, 32, 16, 2],
+	111: [2, 8, 18, 32, 32, 17, 2],
+	112: [2, 8, 18, 32, 32, 18, 2],
+	113: [2, 8, 18, 32, 32, 18, 3],
+	114: [2, 8, 18, 32, 32, 18, 4],
+	115: [2, 8, 18, 32, 32, 18, 5],
+	116: [2, 8, 18, 32, 32, 18, 6],
+	117: [2, 8, 18, 32, 32, 18, 7],
+	118: [2, 8, 18, 32, 32, 18, 8],
+};
+
 var elements = [
 	{
 		name: 'Hydrogen',
@@ -514,9 +635,40 @@ sodiumText.on('mouseleave', function() {
 	sodiumText.fill('white');
 	buttonLayer.draw();
 });
+function stopMove()
+{
+	move.stop();
+}
 
+function haveIntersection(r1, r2) {
+	return !(
+		r2.x > r1.x + r1.width ||
+		r2.x + r2.width < r1.x ||
+		r2.y > r1.y + r1.height ||
+		r2.y + r2.height < r1.y
+	);
+}
+var move = new Konva.Animation(function(frame){
+	var shape = layer.getChildren()[(layer.getChildren().length) - 1];
+	var shapeChild = shape.getChildren()[(shape.getChildren().length) - 1];
+	if(shape.getX() < simulateButton.getX())
+	{
+		shape.setX(shape.getX() + 1);
+	} else
+	{
+		shape.setX(shape.getX() - 1);
+	}
+	if(shape.getY() < simulateButton.getY())
+	{
+		shape.setY(shape.getY() + 1);
+	} else
+	{
+		shape.setY(shape.getY() - 1);
+	}
+}, layer);
 simulateButton.on('click', function(){
 	ionicBonding(elements);
+	move.start();
 });
 
 simulateText.on('click', function(){
@@ -705,39 +857,32 @@ heliumButton.on('click', function() {
 	stroke: 'black',
 	strokeWidth: 1
   });
-  var atom = new Konva.Circle({
-	x: position['x'],
-	y: position['y'],
-	fill: 'red',
-	stroke: 'black',
-	radius: 5,
-	offset: {
-		x: -40,
-		y: 0
-	}
-  });
-  var atom1 = new Konva.Circle({
-	x: position['x'],
-	y: position['y'],
-	fill: 'red',
-	stroke: 'black',
-	radius: 5,
-	offset: {
-		x: 40,
-		y: 0
-	}
-  });
-  var angularSpeed = 90;
-  var anim = new Konva.Animation(function(frame) {
-	  var angleDiff = frame.timeDiff * angularSpeed / 1000;
-	  atom.rotate(angleDiff);
-	  atom1.rotate(angleDiff);
-  }, layer);
   group.add(molecule);
   group.add(simpleText);
   group.add(ringMolecule);
-  group.add(atom);
-  group.add(atom1);
+  for(var i = 0; i < 11; i++)
+  {
+	  var atom = new Konva.Circle({
+		x: position['x'],
+		y: position['y'],
+		fill: 'red',
+		stroke: 'black',
+		radius: 5,
+		offset: {
+			x: -40,
+			y: 0
+		},
+		name: 'atom'
+	  });
+	  atom.rotate(40 * i);
+	  group.add(atom);
+  }
+  angularSpeed = 90;
+  var anim = new Konva.Animation(function(frame) {
+	  var angleDiff = frame.timeDiff * angularSpeed / 1000;
+	  group.find('Circle').rotate(angleDiff);
+	  
+  }, layer);
   group.on('mouseover', function(e) {
 	document.body.style.cursor = 'pointer';
 	var node = e.target;
